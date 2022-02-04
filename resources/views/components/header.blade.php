7486a0a3af3cd7b3b01a0e8b327fb9a1e7fa005e
Mario <lombardomario01@gmail.com>
2022-02-04 17:22:49 +0100

    Header completed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,13 @@
 <header class="mb-3">
     Header
+
+    @auth
+        <div class="container-name">
+            Ciao {{ Auth::user()->name }}
+            <span>
+                <a class="btn btn-danger" href="{{ route('logout') }}">LOGOUT</a>
+            </span>
+        </div>
+    @endauth
+
 </header>
