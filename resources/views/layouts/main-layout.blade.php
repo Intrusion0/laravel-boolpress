a2137d39b037ce6e72f1fbc891b6cf798c2aa5c4
Mario <lombardomario01@gmail.com>
2022-02-04 16:54:38 +0100

    Main Layout added

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-boolpress</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+</head>
+<body>
+    
+    <div id="app"> <!-- Lo inserisco per lunedì, in modo che ho già il collegamento con l'istanza di VUE (stessa cosa il js) -->
+
+    @include('components.header')
+
+    @yield('content')
+
+    @include('components.footer')
+
+    </div>
+
+</body>
+</html>
