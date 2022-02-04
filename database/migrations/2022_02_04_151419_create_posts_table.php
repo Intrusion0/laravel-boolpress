e6d7db081608e8f9623cd40c97ed076c71002c17
Mario <lombardomario01@gmail.com>
2022-02-04 16:35:19 +0100

    Posts table created

---
+++ database/migrations/2022_02_04_151419_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('author', 60);
+            $table->text('description')->nullable();
+            $table->date('pubblication_date');
+            $table->integer('like')->unsigned()->default(0);
+            $table->integer('comments')->unsigned()->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
