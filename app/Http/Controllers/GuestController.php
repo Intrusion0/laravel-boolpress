9532e138ef496ff5258e983a89fc300ccb76ff7a
Mario <lombardomario01@gmail.com>
2022-02-07 14:56:40 +0100

    Controller Refactoring

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -13,25 +13,4 @@ class GuestController extends Controller
 
         return view('pages.home', compact('posts'));
     }
-
-    public function create() {
-
-        return view('pages.create');
-    }
-
-    public function store(Request $request) {
-
-        $data = $request->validate([
-            'title' => 'required|string',
-            'author' => 'required|string',
-            'description' => 'nullable|string',
-            'pubblication_date' => 'required|date',
-            'like' => 'required|integer|min:0|max:1000000',
-            'comments' => 'required|integer|min:0|max:1000000'
-        ]);
-
-        Post::create($data);
-
-        return redirect()->route('home');
-    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,26 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function create() {
+
+        return view('pages.create');
+    }
+
+    public function store(Request $request) {
+
+        $data = $request->validate([
+            'title' => 'required|string',
+            'author' => 'required|string',
+            'description' => 'nullable|string',
+            'pubblication_date' => 'required|date',
+            'like' => 'required|integer|min:0|max:1000000',
+            'comments' => 'required|integer|min:0|max:1000000'
+        ]);
+
+        Post::create($data);
+
+        return redirect()->route('home');
     }
 }
